4402adbdfe95eb6aee6eb45dae7bdd757eac8546
Tsepang Maseti <maseti.tsepang@gmail.com>
2022-11-09 09:17:37 +0200

    initial commit

--- frontend/Fine/src/app/_services/auth.service.ts
+++ frontend/Fine/src/app/_services/auth.service.ts
@@ -4,7 +4,8 @@ import { Observable } from 'rxjs';const AUTH_API ='http://localhost:8050/api/aut
 const httpOptions = {
 headers: new HttpHeaders({ 'Content-Type': 'application/json' })};
 @Injectable({
-providedIn: 'root'})export class AuthService {
+providedIn: 'root'})
+export class AuthService {
 constructor(private http: HttpClient) { }
   login(username: string, password: string): Observable<any> {
     return this.http.post(AUTH_API + 'signin', {
